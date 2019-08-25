ccc1bd78d2f489bfabf7442515203e1c7184e980
emilianocarvalho <emilianocarvalho.dev@gmail.com>
2019-08-25 15:38:38 -0300

    ajustando o gitignore

--- bin/sql/insert.sql
+++
@@ -1,6 +0,0 @@
-create database if not exists fj21;
-
-use fj21;
-
-insert into contatos (nome, email, endereco, dataNascimento)
-values ("Teste", "email@gmail.com", "endereço teste", "07/09/1970");
